33dfce730167b5f098b2d2909db2faae2c72cb8b
dingpeng <peng.ding@ele.me>
2016-12-27 00:32:38 +0800

    kafka test

---
+++ src/test/java/kafkaTest/KafkaBroker.java
@@ -0,0 +1,123 @@
+package kafkaTest;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.Properties;
+
+import kafka.SortedProperties;
+import org.I0Itec.zkclient.ZkClient;
+import org.apache.curator.test.TestingServer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.io.Resources;
+
+import kafka.admin.TopicCommand;
+import kafka.server.KafkaConfig;
+import kafka.server.KafkaServerStartable;
+import kafka.utils.ZKStringSerializer$;
+import kafka.utils.ZkUtils;
+
+public class KafkaBroker {
+
+	private static Logger logger = LoggerFactory.getLogger(KafkaBroker.class);
+
+	private TestingServer zkServer = null;
+
+	public KafkaConfig kafkaConfig = null;
+
+	public KafkaServerStartable kafkaServer = null;
+
+	/**
+	 * default constructor
+	 */
+	public KafkaBroker() {
+		this(createProperties());
+	}
+
+	public KafkaBroker(Properties properties) {
+		try {
+			// Create and run zookeeper
+			zkServer = new TestingServer(2181, true);
+
+			// add zookeeper connect string to the properties before setting
+			// kafkaconfig
+			properties.put("zookeeper.connect", zkServer.getConnectString());
+			kafkaConfig = new KafkaConfig(properties);
+
+			// create and start kafka with kafkaconfig
+			kafkaServer = new KafkaServerStartable(kafkaConfig);
+			kafkaServer.startup();
+		} catch (Exception e) {
+			logger.error("Problem starting broker: ", e);
+		}
+
+		logger.info("Embedded kafka is up");
+	}
+
+	private static SortedProperties createProperties() {
+		SortedProperties properties = null;
+		try (InputStream props = Resources.getResource("kafkaBroker.properties").openStream()) {
+			properties = new SortedProperties();
+			properties.load(props);
+
+			if (logger.isDebugEnabled()) {
+				logger.debug("Start List of Producer specified properties");
+				for (Enumeration<Object> e = properties.keys(); e.hasMoreElements();) {
+					Object o = e.nextElement();
+					logger.debug("Property: {} Value: {}", o, properties.get(o));
+				}
+				logger.debug("End   List of Producer specified properties");
+			}
+		} catch (IOException e) {
+			logger.error("problem loading properties: ", e);
+		}
+		return properties;
+	}
+
+	/**
+	 * Stop of the broker means - stop kafkaServer - stop zookeeper
+	 */
+	public void stop() {
+
+		if (kafkaServer != null) {
+			kafkaServer.shutdown();
+		}
+
+		try {
+			if (zkServer != null) {
+				zkServer.stop();
+			}
+		} catch (IOException e) {
+			logger.error("Error shutdown kafka: ", e);
+		} finally {
+			kafkaServer = null;
+			zkServer = null;
+		}
+
+		logger.info("embedded kafka stop");
+	}
+
+	public KafkaConfig getConfig() {
+		KafkaConfig kc = null;
+
+		if (kafkaServer != null) {
+			kc = kafkaServer.serverConfig();
+		}
+		return kc;
+	}
+	
+	public void createTopic(String topic, String numberPartitions) {
+		logger.info("Begin CreateTopic: {} with number partitions: {}", topic, numberPartitions);
+		String[] arguments = new String[] { "--topic", topic, "--partitions", numberPartitions, "--replication-factor", "1" };
+
+		ZkClient zkClient = new ZkClient(zkServer.getConnectString(), 30000, 30000, ZKStringSerializer$.MODULE$);
+		ZkUtils zkutils = ZkUtils.apply(zkClient, false);
+		
+
+		// create topic
+		TopicCommand.createTopic(zkutils, new TopicCommand.TopicCommandOptions(arguments));
+		logger.info("End   CreateTopic: {} with number partitions: {}", topic, numberPartitions);
+	}
+}
---
+++ src/test/java/kafkaTest/TestKafka.java
@@ -0,0 +1,208 @@
+package kafkaTest;
+
+
+import java.util.UUID;
+
+import kafka.Receiver;
+import kafka.Sender;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TestName;
+import org.junit.runners.MethodSorters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import kafka.server.KafkaConfig;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestKafka {
+	private static Logger logger = LoggerFactory.getLogger(TestKafka.class);
+
+	private static int MAX_MESSAGES = 20;
+	private static int MAX_PARTITION = 4;
+	private static String my_topic = "my-topic";
+	private static String my_part_topic = "my-part-topic";
+
+	private KafkaBroker kb = null;
+	
+	@Rule
+	public TestName name = new TestName();
+
+	@Before
+	public void startKafka() {
+		logger.info("Begin start kafka: {}", name.getMethodName());
+
+		kb = new KafkaBroker();
+		KafkaConfig kc = kb.getConfig();
+		
+		logger.info("advertisedHostName: {}", kc.advertisedHostName());
+		logger.info("authorizerClassName: {}", kc.authorizerClassName());
+		logger.info("compressionType: {}", kc.compressionType());
+		logger.info("hostName: {}", kc.hostName());
+		logger.info("interBrokerProtocolVersionString: {}",kc.interBrokerProtocolVersionString());
+		logger.info("logCleanupPolicy: {}",kc.logCleanupPolicy());
+		logger.info("logMessageFormatVersionString: {}",kc.logMessageFormatVersionString());
+		logger.info("saslKerberosKinitCmd: {}", kc.saslKerberosKinitCmd());
+		logger.info("saslKerberosServiceName: {}", kc.saslKerberosServiceName());
+		logger.info("saslMechanismInterBrokerProtocol: {}", kc.saslMechanismInterBrokerProtocol());
+		logger.info("sslClientAuth: {}", kc.sslClientAuth());
+		
+		
+		logger.info("End   start kafka: {}", name.getMethodName());
+	}
+	
+	@After
+	public void stopKafka() {
+		logger.info("Begin stop kafka");
+		kb.stop();
+		kb = null;
+		logger.info("End   stop kafka");
+	}
+
+	
+	public void test_A_Sender() {
+		Sender<String,String> kafkaSender = new Sender<String,String>();
+		kafkaSender.startSending();
+
+		int i = 0;
+
+		for (i = 0; i < MAX_MESSAGES; i++) {
+			logger.info("Sending message: {}", i);			
+			String message = Integer.toString(i);
+			kafkaSender.sendMessage(my_topic, message);
+		}
+
+		kafkaSender.stopSending();
+		Assert.assertEquals(MAX_MESSAGES, i);
+	}
+	
+	public void test_B_Receiver() {
+		Receiver<String, String> kafkaReceiver = new Receiver<String, String>();
+		kafkaReceiver.startListening(my_topic);
+
+		kafkaReceiver.getMetrics();
+		
+		int msgnr_tot = 0;
+		int msgnr = 0;
+
+		while (msgnr_tot < MAX_MESSAGES) {
+			msgnr = kafkaReceiver.getMessage(1000);
+			msgnr_tot += msgnr;
+		}
+
+		kafkaReceiver.stopListening();
+		Assert.assertEquals(msgnr_tot, MAX_MESSAGES);
+
+	}
+	
+	@Test
+	public void test_value() {
+		logger.info("Start test: {}", name.getMethodName());
+		
+		test_A_Sender();
+		
+		test_B_Receiver();
+		logger.info("End   test: {}", name.getMethodName());
+	}
+	
+	public void test_KVA_Sender() {
+		Sender<String, String> kafkaSender = new Sender<String, String>();
+		kafkaSender.startSending();
+
+		int i = 0;
+
+		for (i = 0; i < MAX_MESSAGES; i++) {
+			logger.info("Sending message: {}", i);
+			String key = UUID.randomUUID().toString();
+			String message = Integer.toString(i);
+			kafkaSender.sendMessage(my_topic, key, message);
+		}
+
+		kafkaSender.stopSending();
+		Assert.assertEquals(MAX_MESSAGES, i);
+	}
+
+	
+	public void test_KVB_Receiver() {
+		Receiver<String, String> kafkaReceiver = new Receiver<String, String>();
+		kafkaReceiver.startListening(my_topic);
+
+		kafkaReceiver.getMetrics();
+		
+		int msgnr_tot = 0;
+		int msgnr = 0;
+
+		while (msgnr_tot < MAX_MESSAGES) {
+			msgnr = kafkaReceiver.getMessage(1000);
+			msgnr_tot += msgnr;
+		}
+
+		kafkaReceiver.stopListening();
+		Assert.assertEquals(msgnr_tot, MAX_MESSAGES);
+
+	}
+	
+	@Test
+	public void test_key_value() {
+		logger.info("Start test: {}", name.getMethodName());
+		
+		test_KVA_Sender();
+		
+		test_KVB_Receiver();
+		logger.info("End   test: {}", name.getMethodName());
+	}
+	
+	public void test_partitionA_Sender() {
+		Sender<String, String> kafkaSender = new Sender<String, String>();
+		kafkaSender.startSending();
+
+		int i = 0;
+
+		for (i = 0; i < MAX_MESSAGES; i++) {
+			logger.info("Sending message: {}", i);
+			String key = UUID.randomUUID().toString();
+			String message = Integer.toString(i);
+			kafkaSender.sendMessage(my_part_topic, (i % MAX_PARTITION), key, message);
+		}
+
+		kafkaSender.stopSending();
+		Assert.assertEquals(MAX_MESSAGES, i);
+	}
+
+	public void test_partitionB_Receiver() {
+		Receiver<String, String> kafkaReceiver = new Receiver<String, String>();
+		kafkaReceiver.startListening(my_part_topic);
+
+		kafkaReceiver.getMetrics();
+		
+		int msgnr_tot = 0;
+		int msgnr = 0;
+
+		while (msgnr_tot < MAX_MESSAGES) {
+			msgnr = kafkaReceiver.getMessage(1000);
+			msgnr_tot += msgnr;
+		}
+
+		kafkaReceiver.stopListening();
+		Assert.assertEquals(msgnr_tot, MAX_MESSAGES);
+
+	}
+	
+	
+	@Test
+	public void test_partition_value() {
+		logger.info("Start test: {}", name.getMethodName());
+		
+		kb.createTopic(my_part_topic, new Integer(MAX_PARTITION).toString());
+		
+		test_partitionA_Sender();
+		
+		test_partitionB_Receiver();
+		logger.info("End   test: {}", name.getMethodName());
+	}
+		
+}
